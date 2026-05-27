220c1470f1676ba2536f4de24922a14b5fc29a4f
git8flyramn <shuntrou43.gmail.com>
2026-05-27 16:17:33 +0900

    5店舗の値段と重さのグラフの仮完成

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import matplotlib.pyplot as plt
 import matplotlib_fontja
 
+
 plt.rcParams["font.family"] = "IPAexGothic"
+
+
 def plot_data():
-    plt.title("寿司屋の重さと値段グラフ")
+    plt.title("回転寿司チェーン店の15貫あたりの重さと値段")
     plt.xlabel("重さ(g)")
     plt.ylabel("値段(円)")
-    plt.plot(10,150,marker=".",color="blue",label="はま寿司")
+        # 15貫の重さ　値段 15貫
+    
+    plt.plot(438.7,968,marker=".",color="blue",label="はま寿司",ms = 32)
+    print("はま寿司",438.7/968) #1円当たり何グラム
+    plt.plot(421.3,1730,marker=".",color="green",label="平禄寿司",ms = 32)
+    print("平禄寿司",421.3/1730)
+    plt.plot(344.7,920,marker=".",color="red",label="くら寿司",ms = 32)
+    print("くら寿司",344.7/920)
+    plt.plot(407.0,1200,marker=".",color="black",label="スシロー",ms = 32)
+    print("スシロー",407.0/1200)
+    plt.plot(345.0,1120,marker=".",color="purple",label="魚べい",ms = 32)
+    print("魚べい",345.0/1120)
     plt.legend()
     plt.show()
 
+   
+
+
+
+
 plot_data()
+
 
 
