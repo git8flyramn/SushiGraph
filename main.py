090119b4d8e854d8f518866c6750dead0f2d50cd
= <=>
2026-05-29 11:42:08 +0900

    カラーコード追加

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ def plot_data():
     plt.ylabel("値段(円)")
         # 15貫の重さ　値段 15貫
     
-    plt.plot(438.7,968,marker=".",color="blue",label="はま寿司",ms = 32)
+    plt.plot(438.7,968,marker=".",color="#0066FF",label="はま寿司",ms = 32)
     print("はま寿司",438.7/968) #1円当たり何グラム
-    plt.plot(421.3,1730,marker=".",color="green",label="平禄寿司",ms = 32)
+    plt.plot(421.3,1730,marker=".",color="#00CC00",label="平禄寿司",ms = 32)
     print("平禄寿司",421.3/1730)
-    plt.plot(344.7,920,marker=".",color="red",label="くら寿司",ms = 32)
+    plt.plot(344.7,920,marker=".",color="#000000",label="くら寿司",ms = 32)
     print("くら寿司",344.7/920)
-    plt.plot(407.0,1200,marker=".",color="black",label="スシロー",ms = 32)
+    plt.plot(407.0,1200,marker=".",color="#FF0000",label="スシロー",ms = 32)
     print("スシロー",407.0/1200)
-    plt.plot(345.0,1120,marker=".",color="purple",label="魚べい",ms = 32)
+    plt.plot(345.0,1120,marker=".",color="#FFD700",label="魚べい",ms = 32)
     print("魚べい",345.0/1120)
     plt.legend()
     plt.show()
